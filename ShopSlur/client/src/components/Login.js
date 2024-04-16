0e677854ec464befa6187315d79144236e18f57e
Karanjot Singh <113842752+Karanxidhu@users.noreply.github.com>
2024-04-17 04:40:41 +0530

    Update Login.js

--- ShopSlur/client/src/components/Login.js
+++ ShopSlur/client/src/components/Login.js
@@ -18,7 +18,7 @@ const Login = () => {
 
     const handleLogin= async (e)=>{
         e.preventDefault();
-        const response = await fetch("http://localhost:5000/api/auth/login", {
+        const response = await fetch("https://shop-slur-backend.vercel.app/api/auth/login", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
